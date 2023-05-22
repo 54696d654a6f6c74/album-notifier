b26a136132a09f40d78a2bc00019b7b72cb30cde
54696d654a6f6c74 <eniz.h12@gmail.com>
2023-05-22 21:42:49 +0200

    Basic install structure

---
+++ install.py
@@ -0,0 +1,9 @@
+from sys import platform
+from installers import win, linux, osx
+
+if platform == "win32":
+  win.install
+elif platform == "darwin":
+  osx.install()
+elif "linux" in platform:
+    linux.install
